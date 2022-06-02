7d9117a86410efd2bd485b28ed1b80a466554f16
Marcin Szwarc <marcinszwarc@hotmail.com>
2022-06-02 11:20:20 +0200

    Tekstury się powtarzają na powierzchni obiektów

--- scene_mgr.cpp
+++ scene_mgr.cpp
@@ -89,8 +89,8 @@ namespace Scene {
 			GL_RGBA, GL_UNSIGNED_BYTE, (unsigned char*)image.data());
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 		return tex;
 	}
 }
